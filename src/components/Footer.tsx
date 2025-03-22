afd8942525b423ace4ef5d42027d93b03c54dc83
swaroffski <123881132+swaroffski@users.noreply.github.com>
2025-03-22 07:34:59 +0300

    Поменял шрифт, два столба

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-// components/Footer.tsx
 import { Box, Link, Typography } from "@mui/material";
 
 export default function Footer() {
@@ -7,21 +6,25 @@ export default function Footer() {
             sx={{
                 backgroundColor: "#333",
                 color: "white",
-                padding: "20px",
+                padding: "5px 10px", // уменьшенные отступы
                 textAlign: "center",
                 position: "relative",
                 bottom: 0,
                 width: "100%",
-                fontSize: "6px",
+                fontSize: "6px", // мелкий текст
+                display: "flex",
+                justifyContent: "space-between", // текст по краям
+                alignItems: "center",
+                height: "30px", // уменьшенная высота футера
             }}
         >
-            <Typography>
-                Сайт разработан на исходном открытом коде. Javascript + Node.js + react{" "}
+            <Typography sx={{ fontSize: "6px", textAlign: "left" }}>
+                Сайт разработан на исходном открытом коде. Javascript + Node.js + React{" "}
                 <Link href="https://github.com/swaroffski/milana-fan" target="_blank" color="inherit">
                     GitHub
                 </Link>
             </Typography>
-            <Typography>
+            <Typography sx={{ fontSize: "6px", textAlign: "right" }}>
                 Created by{" "}
                 <Link href="https://t.me/existas" target="_blank" color="inherit">
                     Swaroffski
