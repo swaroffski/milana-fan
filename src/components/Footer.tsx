0cd44769be5f20db5d4bd5456cda17c6d06965f4
swaroffski <123881132+swaroffski@users.noreply.github.com>
2025-03-22 07:51:58 +0300

    Меньше сделал, редактировал столбцы

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+// components/Footer.tsx
 import { Box, Link, Typography } from "@mui/material";
 
 export default function Footer() {
@@ -6,30 +7,38 @@ export default function Footer() {
             sx={{
                 backgroundColor: "#333",
                 color: "white",
-                padding: "5px 10px", // уменьшенные отступы
+                padding: "10px 20px", // Сделал ещё меньше по высоте
                 textAlign: "center",
                 position: "relative",
                 bottom: 0,
                 width: "100%",
-                fontSize: "6px", // мелкий текст
+                fontSize: "5px", // Ещё чуть меньше шрифт
                 display: "flex",
-                justifyContent: "space-between", // текст по краям
-                alignItems: "center",
-                height: "30px", // уменьшенная высота футера
+                justifyContent: "space-between",
             }}
         >
-            <Typography sx={{ fontSize: "6px", textAlign: "left" }}>
-                Сайт разработан на исходном открытом коде. Javascript + Node.js + React{" "}
-                <Link href="https://github.com/swaroffski/milana-fan" target="_blank" color="inherit">
+            {/* Левая часть: три строки */}
+            <Box sx={{ textAlign: "left" }}>
+                <Typography>Открытый исходный код</Typography>
+                <Typography>Javascript + Node.js + React</Typography>
+                <Link
+                    href="https://github.com/swaroffski/milana-fan"
+                    target="_blank"
+                    color="inherit"
+                >
                     GitHub
                 </Link>
-            </Typography>
-            <Typography sx={{ fontSize: "6px", textAlign: "right" }}>
-                Created by{" "}
-                <Link href="https://t.me/existas" target="_blank" color="inherit">
-                    Swaroffski
-                </Link>
-            </Typography>
+            </Box>
+
+            {/* Правая часть */}
+            <Box sx={{ textAlign: "right" }}>
+                <Typography>
+                    Created by{" "}
+                    <Link href="https://t.me/existas" target="_blank" color="inherit">
+                        Swaroffski
+                    </Link>
+                </Typography>
+            </Box>
         </Box>
     );
 }
